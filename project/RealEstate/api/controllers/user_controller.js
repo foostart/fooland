b9606e9396f442030adb645a31864091c8bd0f75
PhamPhuong0108 <phamvuhoangphuong@gmail.com>
2018-04-06 11:08:12 +0700

    Commit 3 file: user_model, user_controller, swagger.yaml

---
+++ project/RealEstate/api/controllers/user_controller.js
@@ -0,0 +1,153 @@
+
+'use strict';
+
+// Khai báo pattern model 
+const userModelClass = require("../models/users_model");
+const UserModel = new userModelClass();
+
+// export cac hàm có trong controller
+module.exports = {
+    getUserByID: getUserByID,
+    getAllUsers: getAllUsers,
+    insertUser: insertUser,
+    updateUser: updateUser,
+    deleteUser: deleteUser
+};
+
+// Controller lấy user theo userID có trong database
+function getUserByID(req, res, next) {
+    var userID = req.swagger.params['userID'].value;
+
+    var results = {
+        success: 1,
+        data: [],
+        description: "Find user by userID successful."
+    };
+    
+    UserModel.findById(userID,function (rows) {
+        if (rows != -1) {
+            rows.forEach(row => {
+                var user = {
+                    userID: row.user_id,
+                    userName: row.user_name  
+                };
+
+                results.data.push(user);
+            });
+            res.json(results);
+            } else {
+                results.success = 0;
+                results.description = "Error";
+                res.json(results);
+            }
+        });
+}
+
+// Controller lấy hết tất cả user với user_id va user_name có trong database
+function getAllUsers(req, res, next) {
+    var results = {
+        success: 1,
+        data: [],
+        description: "Find all users successful."
+    };
+
+    UserModel.findAll(function (rows) {
+        if (rows != -1) {
+            rows.forEach(row => {
+                var user = {
+                    userID: row.user_id,
+                    userName: row.user_name  
+                };
+                results.data.push(user);
+            });
+            res.json(results);
+        } else {
+            results.success = 0;
+            results.description = "Error";
+            res.json(results);
+        }
+    });
+}
+
+//Insert new user into database
+function insertUser(req, res, next)
+{
+    var userName = req.swagger.params.payload.value.userName;
+    var userPassword = req.swagger.params.payload.value.userPassword;
+
+    var results = {
+        success: 1,
+        data: [userName, userPassword],
+        description: "Insert new user successful."
+    };
+    
+    if (userName && userPassword) {
+        var datas = [
+            userName,
+            userPassword
+        ];
+
+        UserModel.add(datas, function (success) {
+            if (!success) {
+                results.success = 0;
+                results.description = "Cannot insert user into database !";
+            }
+            res.json(results);
+        });
+    }
+} 
+
+//Controller update user
+function updateUser(req, res, next)
+{
+    var userID = req.swagger.params["userID"].value;
+    var userName = req.swagger.params.payload.value.userName;
+    var userPassword = req.swagger.params.payload.value.userPassword;
+    
+    var results = {
+        success: 1,
+        data: [userName, userPassword],
+        description: "Update user into database successful."
+    };
+   
+    
+    //console.log("Password: ", userPassword);
+    if (userName && userPassword) {
+        var datas = {
+            UserName: userName,
+            UserPassword: userPassword
+        };
+
+        UserModel.update(userID, datas, function (success) {
+            if (!success) {
+                results.success = 0;
+                results.description = "Cannot update user into database !";
+            }
+            //console.log(datas);
+            res.json(results);
+        });
+    }
+} 
+
+//DELETE /Users/{UserID}: Xóa user với userID ra khỏi db
+function deleteUser(req, res, next) {
+    var relsutsJson = {
+        success: 1,
+        description: "Delete user from database successful."
+    };
+
+    var userID = req.swagger.params["userID"].value;
+    
+    UserModel.delete(userID, function(success){
+        if(!success)
+        {
+            relsutsJson.success = 0,
+            relsutsJson.description = "Delete user from database failed."
+        }
+        res.json(relsutsJson);
+    });
+}
+
+
+
+
---
+++ project/RealEstate/api/models/users_model.js
@@ -0,0 +1,86 @@
+const DB = require("../../config/DB");
+const request = require("request");
+
+class UserModel extends DB
+{
+    constructor(){
+        super();
+    }
+
+    /**
+     * Insert data into 'users' table
+     * @param data: Data is an array values ex: ['user_name', 'user_password']
+     * @param callback: callback true or false
+     */
+    add(data, callback){
+        this.executeMySQL("INSERT INTO users(user_name, user_password) VALUES (?)", [data]).then(function(success){
+            callback(success);
+        }).catch(function(err){
+            console.log(err);
+            callback(false);
+        })
+    }
+    
+    /**
+     * Delete data from 'users' table
+     * @param userId: user ID to delete
+     * @param callback: callback true or false
+     */
+    delete(userId, callback){
+        this.executeMySQL("DELETE FROM users WHERE user_id =? ", [userId]).then(function(success){
+            callback(success);
+        }).catch(function(err){
+            console.log(err);
+            callback(false);
+        })
+    }
+
+    /**
+     * Update data at 'users' table
+     * @param userId: user ID to update
+     * @param data: data is an json array ex: {userName: "", userPass: ""}
+     * @param callback: callback true or false
+     */
+    update(userId, data, callback){
+        var userName = data.UserName;
+        var userPass = data.UserPassword;
+         this.executeMySQL("UPDATE users SET user_name = ?, user_password = ? WHERE users.user_id = ? ", [[userName], [userPass], [userId]]).then(function(success){
+            callback(success);
+         }).catch(function(err){
+             console.log(err);
+             callback(false);
+         })
+    }
+    
+    /**
+     * Find data at 'users' table
+     * @param userID : site need to find
+     * @param callback: callback return sites rows
+     */
+    findById(userID, callback){
+        var sql = "SELECT * FROM users WHERE user_id = ?" ;
+        this.queryMySQL(sql, [[userID]]).then(function(rows){
+            callback(rows);
+        }).catch(function(err){
+            console.log(err);
+            callback(-1);
+        })
+    }
+
+    /**
+     * Find all data at 'users' table
+     * @param callback: callback return all users
+     */
+    findAll(callback){
+        this.queryMySQL("SELECT * FROM users").then(function(rows){
+            callback(rows);
+        }).catch(function(err){
+            console.log(err);
+            callback(-1);
+        })
+    }
+    
+}
+
+
+module.exports = UserModel;
